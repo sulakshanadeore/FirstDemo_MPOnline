e83e79e22c65d3d30293118b943f87d54c3489f0
sulakshanadeore <deoresulakshana@gmail.com>
2026-05-22 16:06:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");//println
+        Console.WriteLine("Welcome");
+        Console.Write("Hello again");   
+        Console.WriteLine("  This will print on hello again line");
+        Console.Write("This is new line ");
+        Console.WriteLine(100);
+        Console.WriteLine(int.MinValue);
+        Console.WriteLine(int.MaxValue);
+        Console.WriteLine(DateTime.Now);
+        Console.WriteLine('A');
+        Console.WriteLine(4.14f);
+        Console.WriteLine(float.MinValue);
+        Console.WriteLine(float.MaxValue);
+        decimal avgprice = 4554364564;
+        Console.WriteLine(avgprice);//decimal
+        Console.WriteLine("Decimal data type min value=" +decimal.MinValue);//Printing using Concatenation
+        Console.WriteLine($"Decimal datatype max value ={decimal.MaxValue}");//Printing using interpolation
+        double price = 334434.23423;//
+        Console.WriteLine(price);
+
+
+
+    }
+}
